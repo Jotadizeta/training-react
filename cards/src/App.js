7dd31ff7dd33e8dda8948f03103537b83638b184
Adrian <adrianjuradobenitez@gmail.com>
2023-08-11 12:48:21 +0200

    css ready

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,58 +1,78 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faEnvelope, faDollarSign, faAddressBook, faPhoneFlip, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar,
+  faEnvelope,
+  faDollarSign,
+  faAddressBook,
+  faPhoneFlip,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 import CardWrapper from "./components/CardWrapper";
 import freelancer1 from "./assets/img/freelancer-1.jpg";
 
 function App() {
+  const freelancer = [
+    {
+      id: 0,
+      tittle: "MR",
+      lastName: "ROBERT",
+      role: "UX/UI Designer",
+      fee: 85,
+      reviews: 105,
+    },
+  ];
+
   return (
-    <CardWrapper>
-      <div className="img-container">
-        <div className="img-wrapper">
-          <img src={freelancer1} alt="freelance-one" />
-          <span className="dot"></span>
-        </div>
-      </div>
-      <div className="info-container">
-        <h3>MR ROBERT</h3>
-        <p>UI-UX DESIGN</p>
-        <div className="hire">
-          <div>
-            Rate: <span>$85/hr</span>
+    <div className="card-container">
+      <CardWrapper>
+        <div className="img-container">
+          <div className="img-wrapper">
+            <img src={freelancer1} alt="freelance-one" />
+            <span className="dot"></span>
           </div>
-          <button className="hire-button">Hire Me</button>
         </div>
-        <div className="rating">
-          <div>5.0</div>
-          <div>
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
+        <div className="info-container">
+          <h3>MR ROBERT</h3>
+          <p>UI-UX DESIGN</p>
+          <div className="hire">
+            <div>
+              Rate: <span className="price">$85/hr</span>
+            </div>
+            <button className="hire-button">Hire Me</button>
           </div>
-          <div>(105 Reviews)</div>
-        </div>
-        <div className="icons">
-          <div className="verified">
-            <FontAwesomeIcon icon={faCircleCheck} />
-            <span>Verified</span>
+          <div className="rating">
+            <div>5.0</div>
+            <div>
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+            </div>
+            <div>(105 Reviews)</div>
           </div>
-          <span>
-            <FontAwesomeIcon icon={faDollarSign} />
-          </span>
-          <span>
-            <FontAwesomeIcon icon={faEnvelope} />
-          </span>
-          <span>
-            <FontAwesomeIcon icon={faAddressBook} />
-          </span>
-          <span>
-          <FontAwesomeIcon icon={faPhoneFlip}/>
-          </span>
+          <div className="icons">
+            <div className="verified">
+              <FontAwesomeIcon icon={faCircleCheck} />
+              <span>Verified</span>
+            </div>
+            <span className="circle avaiable">
+              <FontAwesomeIcon icon={faDollarSign} />
+            </span>
+            <span className="circle avaiable">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            <span className="circle not-avaiable">
+              <FontAwesomeIcon icon={faAddressBook} />
+            </span>
+            <span className="circle not-avaiable">
+              <FontAwesomeIcon icon={faPhoneFlip} />
+            </span>
+          </div>
         </div>
-      </div>
-    </CardWrapper>
+      </CardWrapper>
+    </div>
   );
 }
 
