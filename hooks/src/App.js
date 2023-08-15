5cd2a826e6d9112cb4473551206ed78481eb9127
Adrian <adrianjuradobenitez@gmail.com>
2023-08-15 11:17:29 +0200

    first example

---
+++ hooks/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Person from './component/Person';
+
+
+function App() {
+  // const [count, setCount]=useState(0);
+  return (
+    <div>
+      <Person></Person>
+      {/* <p>Has clicado {count} veces</p>
+      <button onClick={()=>{
+        setCount(count +1)
+      }}>Pulsame</button> */}
+
+    </div>
+  );
+}
+
+export default App;
