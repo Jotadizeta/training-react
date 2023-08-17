ed4fd0fb59cf8e3a8d96474c037d5f38af01361a
Adrian <adrianjuradobenitez@gmail.com>
2023-08-17 20:20:35 +0200

    first js Expenses ready

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -1,17 +1,11 @@
-// import './App.css';
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio,
-        deleniti distinctio vero maiores rerum delectus ipsum sint quibusdam a
-        modi repellat ducimus hic nam totam quisquam ex eveniet aliquam aut
-        voluptates, tempora exercitationem perspiciatis. Eaque amet deserunt,
-        quam, perspiciatis nesciunt numquam illo facilis, quaerat nobis quos
-        repudiandae ipsum rem consequatur.
-      </p>
+    <div className="main">
+      <h1>Transactions</h1>
+      <Expenses></Expenses>
     </div>
   );
 }
